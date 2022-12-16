091d923d80af839240ed250dc6d124b253aa5028
Vitaly Drogan <vitaliy.drogan@gmail.com>
2022-12-16 17:32:07 +0200

    mev sim sol

--- contracts/src/MevSim.sol
+++ contracts/src/MevSim.sol
@@ -1,7 +1,36 @@
 pragma solidity >=0.8.0;
 
 contract MevSim {
-    function simulate() public pure returns (uint256) {
-        return 42;
+    error BlockMismatch();
+    error SlotValueMismatch();
+
+    function getSlot(uint256 slot) public view returns (uint256) {
+        uint256 value;
+        assembly {
+            value := sload(slot)
+        }
+        return value;
+    }
+
+    function auction(uint256 slot, uint256 value, uint256 target_block) public payable {
+        // check target_block
+        if (block.number != target_block) {
+            revert BlockMismatch();
+        }
+
+        // read current slot value
+        uint256 current = getSlot(slot);
+        if (current != value) {
+            revert SlotValueMismatch();
+        }
+
+        uint256 new_value = value + 1;
+        assembly {
+            sstore(slot, new_value)
+        }
+
+        // send all eth to coinbase
+        address payable coinbase = payable(block.coinbase);
+        coinbase.transfer(address(this).balance);
     }
 }
--- contracts/src/MevSim.t.sol
+++ contracts/src/MevSim.t.sol
@@ -1,10 +1,20 @@
 pragma solidity >=0.8.0;
 
 import "forge-std/Test.sol";
+import "./MevSim.sol";
 
 contract MevSimTest is Test {
+    MevSim mevSim;
+
+    function setUp() public {
+        // deploy MevSim
+        mevSim = new MevSim();
+    }
+
     function testSimulate() public {
-        assertTrue(true);
+        uint value = mevSim.getSlot(1);
+        mevSim.auction{ value: 1}(1, value, block.number);
+        assertEq(mevSim.getSlot(1), value + 1);
     }
 }
 
